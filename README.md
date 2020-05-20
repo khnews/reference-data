87303ea7333e98fcfb86e7c9f037bf53b23794a8
hrecht <hrecht@users.noreply.github.com>
2020-05-20 11:45:29 -0400

    add 1980s state pops

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Basic for use across projects, including FIPS codes and state population.
 Data sources: Census Bureau Population Estimates Program
 * 2000-2010 and 2010-present intercensals via API
 * 1990-2000 intercensals via scraped PDF
+* 1900-1990 intercensals https://www.census.gov/data/tables/time-series/demo/popest/pre-1980-state.html
--- scripts/get-census-demographics.R
+++ scripts/get-census-demographics.R
@@ -2,11 +2,13 @@
 
 library(censusapi)
 library(dplyr)
+library(tidyr)
+library(stringr)
 
 fips_states <- read.csv("geography-codes/fips-states.csv", stringsAsFactors = F, colClasses = "character")
 
 ###########################################################################
-# State population 
+# State population via API
 # Variable and endpoint names aren't totally consistent across vintages
 ###########################################################################
 # Latest years
@@ -42,9 +44,12 @@ state_pop_2000 <- state_population_2000 %>%
 	select(fips_state = state, year, population) %>%
 	arrange(fips_state, year)
 
+###########################################################################
 # 1990-2000 Intercensals totals aren't in data.census.gov, only county-level with demographics in API
-# Scraped PDF from https://www2.census.gov/programs-surveys/popest/tables/1990-2000/intercensal/st-co/co-est2001-12-00.pdf
-state_pop_1990 <- read.csv("pdf/tabula-co-est2001-12-00.csv", stringsAsFactors = F)
+# Scraped PDF from:
+# https://www2.census.gov/programs-surveys/popest/tables/1990-2000/intercensal/st-co/co-est2001-12-00.pdf
+###########################################################################
+state_pop_1990 <- read.csv("data-original/population/tabula-co-est2001-12-00.csv", stringsAsFactors = F)
 state_pop_1990 <- state_pop_1990 %>% gather(year, population, -Geography) %>%
 	rename(state_name = Geography) %>%
 	filter(!(year %in% c("April.1..1990", "April.1..2000.Census"))) %>%
@@ -57,24 +62,36 @@ state_pop_1990 <- state_pop_1990 %>% gather(year, population, -Geography) %>%
 state_pop_1990 <- left_join(state_pop_1990, fips_states, by = "state_name") %>%
 	select(-state_code, -state_name)
 
+###########################################################################
+# 1900-1990 via 
+# https://www.census.gov/data/tables/time-series/demo/popest/pre-1980-state.html
+###########################################################################
+
+state_pop_1980_a <- read.table("data-original/population/st8090ts.txt",
+									 skip = 10, nrows = 52, header = F, stringsAsFactors = F)
+
+state_pop_1980_b <- read.table("data-original/population/st8090ts.txt",
+												 skip = 69, nrows = 52, header = F, stringsAsFactors = F)
+colnames(state_pop_1980_a) <- c("state_code", "4/80cen", "7/81", "7/82", "7/83", "7/84")
+colnames(state_pop_1980_b) <- c("state_code", "7/85" ,"7/86", "7/87", "7/88", "7/89", "4/90cen")
+
 # Join
-state_population <- bind_rows(state_pop_1990, state_pop_2000, state_pop)
+state_pop_1980 <- left_join(state_pop_1980_a, state_pop_1980_b, by = "state_code")
+state_pop_1980 <- state_pop_1980 %>% gather(year, population, -state_code) %>%
+	filter(year != "4/90cen") %>%
+	mutate(year = 1900 + as.numeric(str_replace_all(year, "7/|4/|cen", ""))) %>%
+	filter(state_code != "US")
+
+# Add fips codes
+state_pop_1980 <- left_join(state_pop_1980, fips_states, by = "state_code") %>%
+	select(-state_code, -state_name)
+
+###########################################################################
+# Join years
+###########################################################################
+state_population <- bind_rows(state_pop_1980, state_pop_1990, state_pop_2000, state_pop)
 state_population <- left_join(state_population, fips_states, by = "fips_state")
-state_population <- state_population %>% select(fips_state, state_code, state_name, year, population) %>%
+state_population <- state_population %>% select(year, fips_state, state_code, state_name, population) %>%
 	arrange(fips_state, year)
-	
-write.csv(state_population, "data/state-population.csv", row.names = F, na ="")
 
-###########################################################################
-# County population
-###########################################################################
-population <- getCensus(name = "pep/population",
-												vintage = 2019,
-												vars = c("POP", "DATE_DESC"),
-												region = "county:*")
-population <- population %>% 
-	mutate(fips_county = paste(state, county, sep = ""),
-				 population = as.numeric(POP)) %>%
-	select(fips_county, population)
-
-write.csv(population, "data/county-parts/county-population.csv", row.names = F, na ="")
+write.csv(state_population, "population/state-population.csv", row.names = F, na ="")
